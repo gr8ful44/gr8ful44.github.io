fa917c5d9b74c403519d93043ff992a76dc5c35b
Laphat <120095357+gr8ful44@users.noreply.github.com>
2023-01-21 17:03:58 +0700

    Add files via upload
    change the page.

--- script.js
+++ script.js
@@ -1,193 +1,87 @@
-gsap.registerPlugin(ScrollTrigger);
-gsap.from(".imgWrapper", {
-   scrollTrigger: ".imgWrapper",
-   start: "top top",
-   ease: "expo.out",
-   opacity: 0,
-   duration: 2,
-   scrub: 1,
-});
-gsap.from(".myNameWrapperLeft", {
-   scrollTrigger: ".myNameWrapperLeft",
-   start: "top top",
-   ease: "circ.out",
-   x: -500,
-   delay: 0.2,
-   duration: 1.1,
-   scrub: 1,
-});
-gsap.from(".myNameWrapperRight", {
-   scrollTrigger: ".myNameWrapperRight",
-   start: "top top",
-   ease: "power4.out",
-   x: 500,
-   delay: 0.4,
-   duration: 1.4,
-   scrub: 1,
-});
-gsap.from(".myName", {
-   scrollTrigger: ".myName",
-   ease: "power2.out",
-   y: 50,
-   opacity: 0,
-   delay: 1,
-   duration: 1.2,
-   scrub: 1,
-});
-gsap.from(".myPos", {
-   scrollTrigger: ".myPos",
-   y: 30,
-   opacity: 0,
-   delay: 1.2,
-   scrub: 1,
-});
-gsap.from(".wcText", {
-   scrollTrigger: ".wcText",
-   opacity: 0,
-   delay: 1.6,
-   scrub: 1,
-});
+const parallax = document.querySelector('.parallax')
+window.addEventListener('scroll', () => {
+   let offset = window.pageYOffset;
+   parallax.style.backgroundPositionY = offset * 0.6 + "px";
+})
 
-let gstl = gsap.timeline();
-gstl
-   .to(".myName", {
-      scrollTrigger: {
-         trigger: ".myName",
-         start: "bottom center",
-         scrub: 2,
-      },
-      scale: 3,
-      duration: 1,
-   })
-   .to(".sectionOne", {
-      scrollTrigger: {
-         trigger: ".sectionOne",
-         start: "bottom center",
-         end: "bottom top",
-         scrub: 2,
-      },
-      scale: 2,
-      y: -500,
-      opacity: 0,
-      duration: 3,
-   })
-   .to(".myImg", {
-      scrollTrigger: {
-         trigger: ".myImg",
-         start: "top top",
-         end: "center top",
-         scrub: 1.5,
-      },
-      x: 900,
-      rotate: 360,
-      scale: 2.4,
-      opacity: 0,
-   })
-   .from(".boutMeTitle", {
-      scrollTrigger: {
-         trigger: ".boutMeTitle",
-         start: "top center",
-         end: "+=60px",
-         scrub: 3,
-      },
-      x: 500,
-      scale: 2,
-   })
-   .from(".bio", {
-      scrollTrigger: {
-         trigger: ".bio",
-         start: "top center",
-         end: "+=120px",
-         scrub: 2,
-      },
-      y: 50,
-      opacity: 0,
-   })
-   .from(".certiTitle", {
-      scrollTrigger: {
-         trigger: ".certiTitle",
-         start: "top center",
-         end: "bottom center",
-         scrub: 4,
-      },
-      y: -20,
-      scale: 4,
-   })
-   .from(".li1", {
-      scrollTrigger: {
-         trigger: ".li1",
-         start: "top center",
-         end: "bottom center",
-         scrub: 2.3,
-      },
-      x: 500,
-   })
-   .from(".li2", {
-      scrollTrigger: {
-         trigger: ".li2",
-         start: "top center",
-         end: "bottom center",
-         scrub: 2.3,
-      },
-      x: 500,
-   })
-   .from(".li3", {
-      scrollTrigger: {
-         trigger: ".li3",
-         start: "top center",
-         end: "bottom center",
-         scrub: 2.3,
-      },
-      x: 500,
-   })
-   .from(".li4", {
-      scrollTrigger: {
-         trigger: ".li4",
-         start: "top center",
-         end: "bottom center",
-         scrub: 2.3,
-      },
-      x: 500,
-   })
-   .from(".myWork", {
-      scrollTrigger: {
-         trigger: ".myWork",
-         start: "top center",
-         end: "top top",
-         scrub: 1,
-      },
-      scale: 3,
-   })
-   .from(".desc1", {
-      scrollTrigger: {
-         trigger: ".desc1",
-         start: "top center",
-         end: "center center",
-         scrub: 2,
-      },
-      x: -200,
-      scale: 3,
-      opacity: 0,
-   })
-   .from(".desc2", {
-      scrollTrigger: {
-         trigger: ".desc2",
-         start: "top center",
-         end: "center center",
-         scrub: 2,
-      },
-      x: 500,
-      scale: 3,
-      opacity: 0,
-   })
-   .from(".pageFour", {
-      scrollTrigger: {
-         trigger: ".pageFour",
-         start: "top center",
-         end: "top top",
-         scrub: 1,
-      },
-      y: -40,
-      scale: 1.5,
-      opacity: 0,
-   });
+gsap.registerPlugin(ScrollTrigger);
+gsap.from(".h1-header", {
+   opacity: 0,
+   y: 10,
+   delay: .4,
+})
+gsap.from(".work-title", {
+   scrollTrigger: {
+      trigger: ".work-title",
+      start: "top center",
+      end: "+=150",
+      scrub: 4,
+      once: true,
+   },
+   opacity: 0,
+   x: 100,
+})
+gsap.from(".span-work-title", {
+   scrollTrigger: {
+      trigger: ".span-work-title",
+      start: "top center",
+      end: "+=40",
+      scrub: 2,
+      once: true,
+   },
+   opacity: 0,
+})
+gsap.from(".myimg", {
+   scrollTrigger: {
+      trigger: ".myimg",
+      start: "top center",
+      end: "+=200",
+      scrub: 1,
+      once: true,
+   },
+   opacity: 0,
+   x: -20,
+})
+gsap.from(".mybio", {
+   scrollTrigger: {
+      trigger: ".mybio",
+      start: "top center",
+      end: "+=200",
+      scrub: 1,
+      once: true,
+   },
+   opacity: 0,
+   x: 20,
+})
+gsap.from(".bio-title", {
+   scrollTrigger: {
+      trigger: ".bio-title",
+      start: "top center",
+      end: "+=150",
+      scrub: 4,
+      once: true,
+   },
+   opacity: 0,
+   y: 40,
+})
+gsap.from(".h1-certi", {
+   scrollTrigger: {
+      trigger: ".h1-certi",
+      start: "top center",
+      end: "+=80",
+      scrub: 5,
+      once: true,
+   },
+   scale: 6
+})
+gsap.from(".span-info", {
+   scrollTrigger: {
+      trigger: ".span-info",
+      start: "top center",
+      end: "+=50",
+      scrub: 2,
+      once: true,
+   },
+   opacity: 0,
+   y: -40
+})
